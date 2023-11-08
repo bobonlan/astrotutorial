814971add7b564f8e1d91fed4a557cc748470ec0
bobonlan <117478386+bobonlan@users.noreply.github.com>
2023-11-08 14:03:25 -0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://merry-manatee-576ec8.netlify.app/",
+});
