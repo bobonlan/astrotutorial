d8a2454fcdae5ab96f50555360e9c2788ea30e07
bobonlan <117478386+bobonlan@users.noreply.github.com>
2023-11-08 15:15:49 -0600

    Finish  Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://merry-manatee-576ec8.netlify.app/",
+  integrations: [preact()]
 });
