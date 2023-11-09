9a93ddaad858384f3dec92ac5be7b0c287ddd03a
bobonlan <117478386+bobonlan@users.noreply.github.com>
2023-11-09 13:45:19 -0600

    Gretting fix

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,7 +9,7 @@ export default function Greeting({ messages }) {
   return (
     <div>
       <h3>{greeting}! Thank you for watching!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Gretting</button>
+      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
     </div>
   );
 }
